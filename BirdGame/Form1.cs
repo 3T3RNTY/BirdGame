ef1414493b9448e83b5f615ae255c1d70b4824c8
Emirhan <bayrakemirhan9@gmail.com>
2024-11-02 10:38:03 +0300

    Adding Comments

--- BirdGame/Form1.cs
+++ BirdGame/Form1.cs
@@ -1,10 +1,17 @@
 namespace BirdGame
 {
+
+    // ----- Babam Saðolsun ----- //
+
     public partial class Form1 : Form
     {
+        // Vertical Movement speed
         int verticalMove = 8;
+        // Horizontal Movement Speed
         int horizontalMove = -12;
+        // Cloud Movement Speed
         int cloudSpeed = -10;
+        // Score
         int score = 0;
 
 
@@ -15,21 +22,28 @@ namespace BirdGame
             timer.Stop();
         }
 
+        // When pressed 'Start' button
         private void start_Click(object sender, EventArgs e)
         {
+            // Create necessary things
             PipeCreate();
             Retry();
+
+            // Show hidden objects
             bird.Show();
             pipedown.Show();
             pipeup.Show();
             scoreLabel.Text = "Score: " + score;
             scoreLabel.Show();
+
+            // Start timer
             timer.Start();
 
+            // Hide start button
             start.Hide();
             this.Focus();
         }
-
+        // When pressed down the 'Space' button vertical speed becames negative
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Space)
@@ -37,21 +51,23 @@ namespace BirdGame
                 verticalMove = -8;
             }
         }
-
+        // When stopped pressing 'Space' button vertical speed reverts back
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Space)
             {
                 verticalMove = 8;
-            } 
+            }
         }
-        private void TimerEventProcessor(Object myObject,EventArgs myEventArgs) 
+        // Timer 1 tick = 7 ms
+        private void TimerEventProcessor(Object myObject, EventArgs myEventArgs)
         {
             bird.Top += verticalMove;
             pipeup.Left += horizontalMove;
             pipedown.Left += horizontalMove;
-            cloud.Left += cloudSpeed;   
+            cloud.Left += cloudSpeed;
 
+            // When pipes touches the edge of the screen a new ones will be created
             if (pipeup.Right <= 0)
             {
                 PipeCreate();
@@ -59,16 +75,19 @@ namespace BirdGame
                 scoreLabel.Text = "Score: " + score;
             }
 
+            // When cloud touches the edge of the screen a new one will be created
             if (cloud.Right <= 0)
             {
                 CloudCreate();
             }
 
-            if (score > 5)
+            // When score is above 5 horizontal speed will be increased
+            if (score >= 5)
             {
                 horizontalMove = -15;
             }
 
+            // If Bird touches pipes or edges, game will be over
             if (bird.Bounds.IntersectsWith(pipeup.Bounds) || bird.Bounds.IntersectsWith(pipedown.Bounds) || bird.Bounds.IntersectsWith(ground.Bounds) || bird.Top <= -10)
             {
                 timer.Stop();
@@ -78,8 +97,9 @@ namespace BirdGame
                 start.Focus();
 
             }
-            
+
         }
+        // Pipe creation
         public void PipeCreate()
         {
             Random Random = new Random();
@@ -91,12 +111,14 @@ namespace BirdGame
             pipedown.Location = new Point(1300, pipeupx);
             pipeup.Location = new Point(1300, pipedownx);
         }
+        // Cloud creation
         public void CloudCreate()
         {
             Random Random = new Random();
-            int height = (10 * Random.Next(5, 36));   
-            cloud.Location = new Point(1300,height);
+            int height = (10 * Random.Next(5, 36));
+            cloud.Location = new Point(1300, height);
         }
+        // Resetting necessary variables 
         public void Retry()
         {
             verticalMove = 8;
